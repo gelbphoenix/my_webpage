948348a8c6a3a3c16fca5ddd5f9f095400ff7b8e
Phoenix Paulina Schmid <122753910+gelbphoenix@users.noreply.github.com>
2023-07-02 20:24:31 +0200

    update

--- src/Components/ProjectItem.jsx
+++ src/Components/ProjectItem.jsx
@@ -3,14 +3,14 @@ import { Button } from 'flowbite-react';
 const ProjectItem = ({ title, link }) => {
   return (
     <>
-      <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e] dark:from-gray-600 dark:to-[#4673e4]">
+      <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e] dark:from-gray-600 dark:to-[#4673e4] overflow-hidden object-contain">
         <img
           src={`https://raw.githubusercontent.com/${title}/main/public/picture.webp`}
           alt={title}
           className="rounded-xl group-hover:opacity-10 ease-in duration-150 aspect-square object-cover object-left"
         />
         <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-          <h3 className="text-2xl font-bold text-white tracking-wider text-center">
+          <h3 className="sm:text-base text-2xl font-bold text-white tracking-wider text-center">
             {title}
           </h3>
           <Button
