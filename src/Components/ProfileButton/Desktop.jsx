6cca8d593f4f83f3c1894e36aa1dc3511a8412bd
Phoenix Paulina Schmid <122753910+gelbphoenix@users.noreply.github.com>
2023-07-02 22:44:08 +0200

    Update Desktop.jsx

--- src/Components/ProfileButton/Desktop.jsx
+++ src/Components/ProfileButton/Desktop.jsx
@@ -7,6 +7,7 @@ const DesktopProfile = ({ link, icon, title }) => {
         <Button
           as="a"
           href={link}
+          target="_blank"
           rel={`noopener noreferrer`}
           className="rounded-full shadow-lg text-black dark:text-white bg-gray-200 dark:bg-gray-900 shadow-gray-400 dark:shadow-gray-600 m-2 cursor-pointer hover:scale-110 ease-in duration-200 hover:bg-gray-300"
         >
