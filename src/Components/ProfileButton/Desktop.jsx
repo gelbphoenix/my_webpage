144df3c103ca4390d119a84aa598b3074b42b8e1
Phoenix Paulina Schmid <122753910+gelbphoenix@users.noreply.github.com>
2023-07-02 19:06:56 +0200

    update mastodon verification

--- src/Components/ProfileButton/Desktop.jsx
+++ src/Components/ProfileButton/Desktop.jsx
@@ -1,15 +1,13 @@
 import { Button, Tooltip } from 'flowbite-react';
 
 const DesktopProfile = ({ link, icon, title }) => {
-  const isMastodon = title === 'Mastodon';
-
   return (
     <>
       <Tooltip content={title} placement="left">
         <Button
           as="a"
           href={link}
-          rel={`noopener noreferrer ${isMastodon ? 'me' : ''}`}
+          rel={`noopener noreferrer`}
           className="rounded-full shadow-lg text-black dark:text-white bg-gray-200 dark:bg-gray-900 shadow-gray-400 dark:shadow-gray-600 m-2 cursor-pointer hover:scale-110 ease-in duration-200 hover:bg-gray-300"
         >
           {icon}
--- src/Components/ProfileButton/Mobile.jsx
+++ src/Components/ProfileButton/Mobile.jsx
@@ -1,8 +1,6 @@
 import { Button, Tooltip } from 'flowbite-react';
 
 const MobileProfile = ({ link, icon, title }) => {
-  const isMastodon = title === 'Mastodon';
-
   return (
     <>
       <Tooltip content={title} placement="bottom">
@@ -10,7 +8,7 @@ const MobileProfile = ({ link, icon, title }) => {
           as="a"
           href={link}
           target="_blank"
-          rel={`noopener noreferrer ${isMastodon ? 'me' : ''}`}
+          rel={`noopener noreferrer`}
           className="rounded-full shadow-lg text-black dark:text-white bg-gray-200 dark:bg-gray-900 shadow-gray-400 dark:shadow-gray-600 m-2 cursor-pointer hover:scale-110 ease-in duration-200 hover:bg-gray-300"
         >
           {icon}
