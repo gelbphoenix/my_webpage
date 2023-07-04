a3a8e439d877895b4b6b5122ec0dbfb23be25859
Phoenix Paulina Schmid <122753910+gelbphoenix@users.noreply.github.com>
2023-07-05 01:26:32 +0200

    Update ProjectItem.jsx

--- src/Components/ProjectItem.jsx
+++ src/Components/ProjectItem.jsx
@@ -6,11 +6,11 @@ const ProjectItem = ({ title, link }) => {
       <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e] dark:from-gray-600 dark:to-[#4673e4] overflow-hidden object-contain">
         <img
           src={`https://raw.githubusercontent.com/${title}/main/public/picture.webp`}
-          alt={title}
-          className="rounded-xl group-hover:opacity-10 ease-in duration-150 aspect-square object-cover object-left"
+          alt=""
+          className="rounded-xl group-hover:opacity-10 ease-out duration-100 aspect-square object-cover object-left bg-gray-400 w-full"
         />
         <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-          <h3 className="sm:text-2xl text-base font-bold text-white tracking-wider text-center">
+          <h3 className="md:text-2xl text-base font-bold text-white tracking-wider text-center ">
             {title}
           </h3>
           <Button
@@ -20,7 +20,7 @@ const ProjectItem = ({ title, link }) => {
             rel="noopener noreferrer"
             className=" w-full mt-2 bg-white dark:bg-gray-700 text-gray-700 dark:text-white font-bold hover:bg-gray-200 dark:hover:bg-gray-600"
           >
-            More Info
+            More Infos and Repository
           </Button>
         </div>
       </div>
