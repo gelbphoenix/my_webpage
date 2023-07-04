5e708715711d5cfd33ae5ad800917fc83a5a806b
Phoenix Paulina Schmid <122753910+gelbphoenix@users.noreply.github.com>
2023-07-05 01:37:36 +0200

    Update ProjectItem.jsx

--- src/Components/ProjectItem.jsx
+++ src/Components/ProjectItem.jsx
@@ -3,14 +3,14 @@ import { Button } from 'flowbite-react';
 const ProjectItem = ({ title, link }) => {
   return (
     <>
-      <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e] dark:from-gray-600 dark:to-[#4673e4] overflow-hidden object-contain">
+      <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-400 to-[#001b5e] dark:from-gray-600 dark:to-[#4673e4] overflow-hidden object-contain">
         <img
           src={`https://raw.githubusercontent.com/${title}/main/public/picture.webp`}
           alt=""
           className="rounded-xl group-hover:opacity-10 ease-out duration-100 aspect-square object-cover object-left bg-gray-400 w-full"
         />
         <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-          <h3 className="md:text-2xl text-base font-bold text-white tracking-wider text-center ">
+          <h3 className="lg:text-2xl md:text-xl text-base font-bold text-white tracking-wider text-center ">
             {title}
           </h3>
           <Button
