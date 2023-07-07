2ed44bc27c8e51a997dfe7ca2a016fdf27ba3ec6
Phoenix Paulina Schmid <122753910+gelbphoenix@users.noreply.github.com>
2023-07-08 01:20:55 +0200

    Update ThreadsInsta.jsx

--- src/Components/icons/ThreadsInsta.jsx
+++ src/Components/icons/ThreadsInsta.jsx
@@ -1,7 +1,16 @@
 import { ReactComponent as ThreadsLogo } from '../../Assets/threads.svg';
 
 const ThreadsInsta = ({ size }) => {
-  return <ThreadsLogo width={size} height={size} />;
+  return (
+    <ThreadsLogo
+      width={size}
+      height={size}
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      role="img"
+    />
+  );
 };
 
 export default ThreadsInsta;
