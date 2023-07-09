5f706e04b646b390a70bf3a51f19e732d02d5758
Phoenix Paulina Schmid <122753910+gelbphoenix@users.noreply.github.com>
2023-07-09 15:15:39 +0200

    Update 1.2.7

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './Styles/index.css';
+
 import ThemeProvider from './Context/ThemeContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import 'flowbite';
+
 import SimpleBar from 'simplebar-react';
-import './Styles/index.css';
 import 'simplebar-react/dist/simplebar.min.css';
 import './Styles/simplebar.css';
+
+import ResizeObserver from 'resize-observer-polyfill';
+window.ResizeObserver = ResizeObserver;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={new QueryClient()}>
       <ThemeProvider>
-        <SimpleBar className="max-h-screen">
+        <SimpleBar forceVisible="y" autoHide={false} className="max-h-screen">
           <App />
         </SimpleBar>
       </ThemeProvider>
