72704ddf0d7e6e93ab6f6e1ea3a1441d4b9fb8e3
Phoenix Paulina Schmid <122753910+gelbphoenix@users.noreply.github.com>
2023-07-13 12:05:45 +0200

    Update 1.3.2

--- src/Components/ThemeButton/Desktop.jsx
+++ src/Components/ThemeButton/Desktop.jsx
@@ -1,7 +1,7 @@
-import { Button, Tooltip } from 'flowbite-react';
-import { BsMoonFill, BsSun } from 'react-icons/bs';
-import { ThemeContext } from '../../Context/ThemeContext';
 import { useContext } from 'react';
+import { BsMoonFill, BsSunFill } from 'react-icons/bs';
+import { Button, Tooltip } from 'flowbite-react';
+import { ThemeContext } from '../../Context/ThemeContext';
 
 const DesktopThemeButton = () => {
   const { theme, handleThemeChange } = useContext(ThemeContext);
@@ -12,9 +12,9 @@ const DesktopThemeButton = () => {
       <Tooltip content={themeText} placement="right">
         <Button
           onClick={() => handleThemeChange()}
-          className="rounded-full shadow-lg text-black dark:text-gray-200 bg-stone-300 dark:bg-stone-600 shadow-gray-400 dark:shadow-gray-600 m-2 cursor-pointer hover:scale-110 ease-in duration-200 hover:bg-gray-300"
+          className="rounded-full shadow-lg text-black dark:text-gray-200 bg-stone-300 dark:bg-stone-600 hover:bg-gray-300 dark:hover:bg-gray-600 shadow-gray-400 dark:shadow-gray-600 m-2 cursor-pointer hover:scale-110 ease-in duration-200"
         >
-          {theme ? <BsMoonFill size={20} /> : <BsSun size={20} />}
+          {theme ? <BsMoonFill size={20} /> : <BsSunFill size={20} />}
         </Button>
       </Tooltip>
     </>
