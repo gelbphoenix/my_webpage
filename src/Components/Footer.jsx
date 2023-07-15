3de1853d44fe4bc6980b36facdabbddbab2fbc00
Phoenix Paulina Schmid <122753910+gelbphoenix@users.noreply.github.com>
2023-07-15 16:17:45 +0200

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,22 @@
+import { Button } from "flowbite-react"
+import {AiOutlineMail} from 'react-icons/ai';
+
 const Footer = () => {
   return (
     <>
-        <div className="grid grid-cols-2 gap-2 text-sm">
-          <section className="flex lg:flex-row flex-col justify-start items-center flex-wrap text-gray-500 dark:text-gray-300">
-            &copy; {new Date().getFullYear()} -
+        <div className="grid grid-cols-3 gap-2 pb-2 text-sm">
+          <section className="flex lg:flex-row flex-col justify-center items-start content-center flex-wrap text-gray-500 dark:text-gray-300">
+            &copy; {new Date().getFullYear() !== 2023 ? `2023 - ${new Date().getFullYear()}` : new Date().getFullYear()}
             <span className="font-bold lg:ml-1 flex flex-row">
-              Made with<p className="text-red-600 mx-1">{'<3'}</p>by Phoenix Paulina Schmid
+               Made with<p className="text-red-600 lg:mx-1 mx-0.5">{'<3'}</p>by Phoenix Paulina Schmid
             </span>
           </section>
-          <section className="flex lg:flex-row flex-col justify-between items-end gap-1 text-gray-500 dark:text-gray-300">
+          <section className="flex lg:flex-row flex-col justify-center items-center flex-wrap text-gray-500 dark:text-gray-300">
+            <Button as="a" href="#contact" className="rounded-full shadow-lg text-stone-200 dark:text-black bg-[#001b5e] dark:bg-[#4673e4] shadow-gray-400 dark:shadow-gray-600 m-2 p-1 cursor-pointer hover:scale-110 ease-in duration-200 hover:bg-[#001b7c] dark:hover:bg-[#4674c6] font-extrabold uppercase">
+              <AiOutlineMail size={20} />
+            </Button>
+          </section>
+          <section className="flex lg:flex-row flex-col flex-wrap justify-between items-end content-center text-gray-500 dark:text-gray-300">
             <a
               href="#"
               className="hover:underline"
