a7373ac76afd08ecc95e39077e31212c7c75f028
Phoenix Paulina Schmid <122753910+gelbphoenix@users.noreply.github.com>
2023-10-03 19:24:46 +0200

    fix focus bug

--- src/Components/ProfileButton/Desktop.jsx
+++ src/Components/ProfileButton/Desktop.jsx
@@ -9,7 +9,7 @@ const DesktopProfile = ({ icon, title, longTitle }) => {
           href={`https://${title.toLowerCase()}.gelbphoenix.de`}
           target="_blank"
           rel={`noopener noreferrer`}
-          className="rounded-full shadow-lg text-black dark:text-gray-200 bg-stone-300 dark:bg-stone-600 hover:bg-gray-300 dark:hover:bg-gray-600 shadow-gray-400 dark:shadow-gray-600 m-2 cursor-pointer hover:scale-110 ease-in duration-200 focus:ring-0"
+          className="rounded-full shadow-lg text-black dark:text-gray-200 bg-stone-300 dark:bg-stone-600 hover:bg-gray-300 dark:hover:bg-gray-600 shadow-gray-400 dark:shadow-gray-600 m-2 cursor-pointer hover:scale-110 ease-in duration-200 focus-visible:ring-0"
         >
           {icon}
         </Button>
--- src/Components/ThemeButton/Mobile.jsx
+++ src/Components/ThemeButton/Mobile.jsx
@@ -9,7 +9,7 @@ const MobileThemeButton = () => {
   return (
     <Button
       onClick={() => handleThemeChange()}
-      className="w-[75%] flex justify-center items-center rounded-full shadow-lg text-black dark:text-white bg-gray-200 dark:bg-gray-900 shadow-gray-400 dark:shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 hover:bg-gray-300 hover:dark:bg-gray-800 focus:ring-0"
+      className="w-[75%] flex justify-center items-center rounded-full shadow-lg text-black dark:text-white bg-gray-200 dark:bg-gray-900 shadow-gray-400 dark:shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 hover:bg-gray-300 hover:dark:bg-gray-800 focus-visible:ring-0"
     >
       {theme ? <BsMoonFill size={20} /> : <BsSun size={20} />}
       <span className="pl-4 text-lg font-bold">
